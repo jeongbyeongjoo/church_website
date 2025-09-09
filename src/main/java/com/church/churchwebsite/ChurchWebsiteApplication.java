aa9e275dedb8b47e2a73eb4a9ab4ae2b6376c211
JeongByeongJoo <jbj030108@icloud.com>
2025-09-09 16:58:04 +0900

    first

---
+++ src/main/java/com/church/churchwebsite/ChurchWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.church.churchwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/church/churchwebsite/ChurchWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.church.churchwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
